377a3e373362866d044f720cdb2b828f6d35a32f
yui.ogasawara <yui.ogasawara@uzabase.com>
2022-06-10 18:21:02 +0900

    id 指定して GET できるようにした

--- src/todo/boundary/todos.clj
+++ src/todo/boundary/todos.clj
@@ -3,13 +3,21 @@
 
 (defprotocol Todos
   (get-todos [db])
+  (find-todo [db id])
   (create-todo [db params]))
 
 (extend-protocol Todos
   duct.database.sql.Boundary
-  
+
   (get-todos [{:keys [spec]}]
     (jdbc/query spec ["SELECT * FROM todos"]))
-  
+
+  (find-todo [{:keys [spec]} id]
+    (jdbc/query spec ["SELECT * FROM todos WHERE id = ?" id]))
+
+  ;; "No such var: jdbc/get-by-id" ってコンパイルエラーが出る
+  ;; (find-todo [{:keys [spec]} id]
+  ;;   (jdbc/get-by-id spec :todos id))
+
   (create-todo [{:keys [spec]} params]
     (jdbc/insert! spec :todos {:title (:title params)})))
--- src/todo/handler/todos.clj
+++ src/todo/handler/todos.clj
@@ -7,11 +7,17 @@
   (fn [_]
     (let [todos (todos/get-todos db)]
       [::response/ok todos]))
- )
+  )
+
+(defmethod ig/init-key ::find [_ {:keys [db]}]
+  (fn [{[_ id] :ataraxy/result}]
+    (let [result (todos/find-todo db id)]
+      [::response/ok result])))
 
 (defmethod ig/init-key ::create [_ {:keys [db]}]
   (fn [{[_ params] :ataraxy/result}]
     (let [result (todos/create-todo db params)
           id (:id (first result))]
       [::response/created (str "/todos/" id)]))
- )
+  )
+
